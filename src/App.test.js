941d72e3ec61893d8282729e571a28b511ca3ef4
Kyle Treman <jkt290@nyu.edu>
2018-05-09 18:05:48 -0400

    Added snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -7,10 +7,17 @@ import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 
 describe("<App />", () => {
+  const wrapper = shallow(<App />)
   it('should render App', () => {
-    const wrapper = shallow(<App />)
-    console.log(wrapper.debug())
-  })
+    // console.log(wrapper.debug())
+  });
+  it('should have a ul with 3 children', () => {
+    expect(wrapper.find('ul').children().length).toBe(3);
+  });
+  it('matches the snapshot', () => {
+    const tree = shallow(<App />)
+    expect(tree).toMatchSnapshot();
+  });
 })
 
 
