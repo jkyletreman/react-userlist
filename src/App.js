01542251843d3cf3ebc465dfa016c869fc288db2
Kyle Treman <jkt290@nyu.edu>
2018-05-09 14:37:52 -0400

    list display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+const userData = [
+  {
+    id: 1,
+    first: "John",
+    last: "Roberts",
+    age: 51,
+    location: "Chicago, IL",
+    description: "John is a retired YouTuber"
+  },
+  {
+    id: 2,
+    first: "Frita",
+    last: "Waters",
+    age: 27,
+    location: "Phoenix, AZ",
+    description: "Frita loves the desert, cheese, and turtles."
+  },
+  {
+    id: 3,
+    first: "Bart",
+    last: "Simpson",
+    age: 24,
+    location: "Springfield, IL",
+    description: "Bart skateboards everywhere."
+  }
+];
 
 class App extends Component {
+
   render() {
     return (
-      <div>1</div>
+      <ul>
+        {userData.map(user => (
+          <li
+            key={user.id} 
+            onClick={this.handleClick}
+            >
+            {user.first} {user.last}
+          </li>
+        ))}
+      </ul>
     );
   }
 }
