d17e1e767829783636b09c4bf65289460839cb7f
Kyle Treman <jkt290@nyu.edu>
2018-05-11 15:50:00 -0400

    Export userdata to json

--- src/UserListPresentation/UserList.test.js
+++ src/UserListPresentation/UserList.test.js
@@ -5,6 +5,7 @@ import Adapter from 'enzyme-adapter-react-16'
 import toJson from 'enzyme-to-json'
 import UserList from './UserList'
 import ShallowRenderer from 'react-test-renderer/shallow';
+const userData = require("../userData.json");
 
 it('renders user shallow correctly', () => {
   const renderer = new ShallowRenderer();
@@ -12,30 +13,3 @@ it('renders user shallow correctly', () => {
   const tree = renderer.getRenderOutput();
   expect(tree).toMatchSnapshot();
 });
-
-const userData = [
-  {
-    id: 1,
-    first: "John",
-    last: "Roberts",
-    age: 51,
-    location: "Chicago, IL",
-    description: "John is a retired YouTuber"
-  },
-  {
-    id: 2,
-    first: "Frita",
-    last: "Waters",
-    age: 27,
-    location: "Phoenix, AZ",
-    description: "Frita loves the desert, cheese, and turtles."
-  },
-  {
-    id: 3,
-    first: "Bart",
-    last: "Simpson",
-    age: 24,
-    location: "Springfield, IL",
-    description: "Bart skateboards everywhere."
-  }
-];
