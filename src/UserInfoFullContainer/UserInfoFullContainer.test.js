46995f38d47d9f93a2ffd79c71ae3f24909b65fb
Kyle Treman <jkt290@nyu.edu>
2018-05-11 15:26:17 -0400

    Add test files and boilerplate

---
+++ src/UserInfoFullContainer/UserInfoFullContainer.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
