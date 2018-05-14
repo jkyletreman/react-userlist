b576f6a915ba35539746ee4dd71b4cc712a952d3
Kyle Treman <jkt290@nyu.edu>
2018-05-14 19:23:58 -0400

    Update test coverage

--- src/UserInfoFullPresentation/UserInfoFull.test.js
+++ src/UserInfoFullPresentation/UserInfoFull.test.js
@@ -4,16 +4,11 @@ import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import toJson from "enzyme-to-json";
 import UserInfoFull from "./UserInfoFull";
+import {selectedUser} from '../data/fixtures'
 
 configure({ adapter: new Adapter() });
-const selectedUser = {
-      first: "kyle",
-      last: "treman",
-      location: "NY",
-      description: "working"
-    }
-const userinfofull = mount(<UserInfoFull birthYear={1989} selectedUser={selectedUser}
-  />
+
+const userinfofull = mount(<UserInfoFull birthYear={1989} selectedUser={selectedUser} />
 );
 const instance = userinfofull.getElement().props;
 
