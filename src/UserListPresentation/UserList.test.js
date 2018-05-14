3f4e98589f7a6d891a6025d4ede7a5729517c69f
Kyle Treman <jkt290@nyu.edu>
2018-05-14 19:30:14 -0400

    Fix path

--- src/UserListPresentation/UserList.test.js
+++ src/UserListPresentation/UserList.test.js
@@ -4,7 +4,7 @@ import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import toJson from "enzyme-to-json";
 import UserList from "./UserList";
-const userData = require("../userData.json");
+const userData = require("../data/userData.json");
 
 configure({ adapter: new Adapter() })
 
