c7f33b64b971188de19b7eb42527f5c358605a99
Kyle Treman <jkt290@nyu.edu>
2018-05-14 18:52:05 -0400

    Redesign test suite

--- src/UserListContainer/UserListContainer.test.js
+++ src/UserListContainer/UserListContainer.test.js
@@ -2,24 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import UserListContainer from "./UserListContainer";
 import UserList from "../UserListPresentation/UserList";
-import { configure, shallow } from "enzyme";
+import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import toJson from "enzyme-to-json";
 const userData = require("../userData.json");
 
 configure({ adapter: new Adapter() });
-const tree = shallow(<UserListContainer />);
+const wrapper = mount(<UserListContainer />);
 
-describe("<UserListContainer />", () => {
-  it("renders shallow correctly", () => {
-    expect(toJson(tree)).toMatchSnapshot();
+describe("UserListcontainer", () => {
+  it("renders without crashing", () => {
+    console.log(wrapper.debug());
   });
 
-  it("should have a default state of false", () => {
-    expect(tree.state().userIsSelected).toBe(false);
+  it('initializes `userIsSelected` as `false`', () => {
+    expect(wrapper.state().userIsSelected).toBe(false)
   });
 
-  // it("should update the state when a user is clicked", () => {
-  //   expect(tree.find(UserList).dive().find('li').simulate('click'))
-  // })
-});
+  it('initializes `userSelected` as ""', () => {
+    expect(wrapper.state().userSelected).toEqual('')
+  })
+
+  describe('on click of user', () => {
+
+    beforeEach(() => {
+      wrapper.find('li').at[0].simulate('click')
+    })
+
+    it("updates `userIsSelected` to `true`", () => {
+      expect(wrapper.state().userIsSelected).toBe(true)
+    })
+  })
+})
