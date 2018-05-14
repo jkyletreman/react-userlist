5d6a0f256060001d1e931c52b520917a88db4899
Kyle Treman <jkt290@nyu.edu>
2018-05-14 19:06:59 -0400

    Improve test suite

--- src/UserListPresentation/UserList.test.js
+++ src/UserListPresentation/UserList.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { configure, shallow } from "enzyme";
+import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import toJson from "enzyme-to-json";
 import UserList from "./UserList";
@@ -8,15 +8,19 @@ const userData = require("../userData.json");
 
 configure({ adapter: new Adapter() })
 
-const tree = shallow(<UserList userData={userData} />);
 
 describe("<UserList />", () => {
+  const userlist = mount(<UserList userData={userData} />);
 
   it("renders shallow correctly", () => {
-    expect(toJson(tree)).toMatchSnapshot();
+    expect(toJson(userlist)).toMatchSnapshot();
+  });
+
+  it('should accept `userData as props`', () => {
+    expect(userlist.props().userData).toEqual(userData)
   });
 
   it("should render a ul with 3 li children", () => {
-    expect(tree.find('ul').children().length).toBe(3);
+    expect(userlist.find('ul').children().length).toBe(3);
   });
 });
