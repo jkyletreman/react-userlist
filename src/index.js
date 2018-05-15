9d46604d43a79da0b60577d415b9f4cbf9b2f301
Kyle Treman <jkt290@nyu.edu>
2018-05-15 13:30:50 -0400

    Add context provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import UserListContainer from "./UserListContainer/UserListContainer"
+import UserDataProvider from "./UserDataProvider/UserDataProvider";
+import UserListContainer from "./UserListContainer/UserListContainer";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<UserListContainer />, document.getElementById('root'));
+ReactDOM.render(
+  <UserDataProvider>
+    <UserListContainer />
+  </UserDataProvider>
+  , document.getElementById('root'));
 registerServiceWorker();
