1e4abe0a96b4ec2fee8ec47f3626a5dba2bb79a8
Kyle Treman <jkt290@nyu.edu>
2018-05-15 13:31:51 -0400

    Create Provider component

---
+++ src/UserDataProvider/UserDataProvider.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import UserData from "../context";
+// const userData = require("../data/userData.json");
+
+
+export default class UserDataProvider extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [
+        {
+          id: 1,
+          first: "John",
+          last: "Roberts",
+          age: 51,
+          location: "Chicago, IL",
+          description: "John is a retired YouTuber"
+        },
+        {
+          id: 2,
+          first: "Frita",
+          last: "Waters",
+          age: 27,
+          location: "Phoenix, AZ",
+          description: "Frita loves the desert, cheese, and turtles."
+        },
+        {
+          id: 3,
+          first: "Bart",
+          last: "Simpson",
+          age: 24,
+          location: "Springfield, IL",
+          description: "Bart skateboards everywhere."
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <UserData.Provider value={this.state.userData}>
+        {this.props.children}
+      </UserData.Provider>
+    );
+  }
+}
