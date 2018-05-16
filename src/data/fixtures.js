63726bc29b342e65b733d0c8d7ff71b35dea576b
Kyle Treman <jkt290@nyu.edu>
2018-05-15 20:30:21 -0400

    Add context test data

--- src/data/fixtures.js
+++ src/data/fixtures.js
@@ -1,6 +1,18 @@
-export const selectedUser = [{
-      first: "kyle",
-      last: "treman",
-      location: "NY",
-      description: "working"
-    }]
+const userData = require("../data/userData.json");
+
+export const selectedUser = [
+  {
+    first: "kyle",
+    last: "treman",
+    location: "NY",
+    description: "working"
+  }
+];
+
+export const context = {
+  state: {
+    selectedUser: "",
+    userIsSelected: false,
+    userData: userData,
+  }
+}
