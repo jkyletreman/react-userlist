99cec9b13b051ee80c5a6595cf5c727f308ef680
Kyle Treman <jkt290@nyu.edu>
2018-05-15 21:20:31 -0400

    Testing configuration for tests

--- src/UserDataProvider/UserDataProvider.test.js
+++ src/UserDataProvider/UserDataProvider.test.js
@@ -1,13 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Enzyme, { configure, mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import toJson from "enzyme-to-json";
 import UserDataProvider from "./UserDataProvider";
 import UserData from "../data/context";
-import { context } from "../data/fixtures";
 const userData = require("../data/userData.json");
-
 
 configure({ adapter: new Adapter() });
 
@@ -15,25 +12,31 @@ beforeEach(() => {
   jest.resetModules();
 });
 
-const getProviderWithContext = (context = {selectedUser: '', userIsSelected: false, userData: userData}) => {
-
+const getProviderWithContext = (
+  context = { selectedUser: "", userIsSelected: false, userData: userData }
+) => {
   // Will then mock the UserData module being used in our LanguageSelector component
-  jest.doMock('../data/context', () => {
+  jest.doMock("../data/context", () => {
     return {
       UserData: {
-        Consumer: (props) => props.children(context)
+        Consumer: props => props.children(context)
       }
-    }
+    };
   });
 
   // you need to re-require after calling jest.doMock.
   // return the updated LanguageSelector module that now includes the mocked context
-  return require('./UserData').UserDataProvider;
+  return require("./UserDataProvider").UserDataProvider;
 };
 
 describe("UserDataProvider", () => {
-  const provider = mount(<UserDataProvider />)
+  const UserDataProvider = getProviderWithContext({
+    selectedUser: "",
+    userIsSelected: false,
+    userData: userData
+  });
+  const provider = mount(<UserDataProvider />);
   it("should render without crashing", () => {
-    console.log(UserDataProvider.debug())
-  })
-})
+    console.log(UserDataProvider.debug());
+  });
+});
