f40fe1f26cdfd078006b6b4dd6de53186dde5bdb
Kyle Treman <jkt290@nyu.edu>
2018-05-15 20:30:37 -0400

    Attempting to refractor for Context Api

--- src/UserList/UserList.test.js
+++ src/UserList/UserList.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { configure, mount } from "enzyme";
+import { configure, mount, shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import toJson from "enzyme-to-json";
 import UserList from "./UserList";
@@ -10,9 +10,10 @@ configure({ adapter: new Adapter() })
 
 
 describe("<UserList />", () => {
-  const userlist = mount(<UserList userData={userData} />);
+  const userlist = shallow(<UserList />);
 
   it("renders shallow correctly", () => {
+    console.log(userlist.debug())
     expect(toJson(userlist)).toMatchSnapshot();
   });
 
